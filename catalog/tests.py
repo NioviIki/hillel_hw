f91364e9666dea9411cd5a882a62f8dcecd91549
niovi <prodamgarazhnedorogo@gmail.com>
2023-02-07 15:47:58 +0200

    test

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from catalog.management.commands.create_users import Command
+
+class UserModelTests(TestCase):
+
+    def test_a_user_has_been_created_or_not(self):
+
+        """Пробует создать 5 пользователей"""
+
+        test_create = Command()
+        test_create.handle(user_create=5)
+        check =  User.objects.count() > 0
+
+        self.assertIs(check, True)
+
+
+
