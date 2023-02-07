003759ddc8e660e56ffc94295fc8827fb508a472
niovi <prodamgarazhnedorogo@gmail.com>
2023-02-07 15:02:05 +0200

    created user is successful

--- catalog/management/commands/create_users.py
+++ catalog/management/commands/create_users.py
@@ -1,17 +1,19 @@
-from faker import Faker
-from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from faker import Faker
+
 
 class Command(BaseCommand):
-    help = "Creates a random user with username, email, password."
+
     def add_arguments(self, parser):
-        parser.add_argument('user_create', type=int, choices=range(1, 10))
+        parser.add_argument('user_create', type=int, choices=range(1, 10),
+                            help="Creates a random user with username, email, password.")
 
     def handle(self, *args, **options):
         fake = Faker()
 
-        list_of_fake = [[fake.unique.user_name(), fake.unique.email(), fake.unique.password()] for _ in range(options["user_create"])]
-        # self.stdout.write(self.style.SUCCESS(f'Successfully {options["user_create"]}, {list_of_fake}'))
-        # self.stdout.write(f"{User.objects.create(username='pepa')}")
-        for i in list_of_fake:
-            self.stdout.write(f"{User.objects.create(username=i[0], email=i[1], password=i[2]),}")
+        for i in range(options["user_create"]):
+            User.objects.bulk_create([User(username=fake.unique.user_name(),
+                                           email=fake.unique.email(),
+                                           password=fake.unique.password())])
