3183c76b0b726fc08be51dd8d44c2c655e0f9cc1
niovi <prodamgarazhnedorogo@gmail.com>
2023-02-07 13:02:29 +0200

    created list with random val

--- catalog/management/commands/create_users.py
+++ catalog/management/commands/create_users.py
@@ -1,8 +1,12 @@
+from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('poll_ids', nargs='?')
+        parser.add_argument('user_create', type=int, choices=range(1, 10))
 
     def handle(self, *args, **options):
-            self.stdout.write(self.style.SUCCESS('Successfully'))
+        fake = Faker()
+
+        list_of_fake = [[fake.unique.user_name(), fake.unique.email(), fake.unique.password()] for _ in range(options["user_create"])]
+        self.stdout.write(self.style.SUCCESS(f'Successfully {options["user_create"]}, {list_of_fake}'))
