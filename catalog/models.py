50135b4ed9b7a24a6f2a9af908a264836904cac9
niovi <prodamgarazhnedorogo@gmail.com>
2023-02-11 13:21:28 +0200

    final

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class City(models.Model):
     city = models.CharField(max_length=30)
 
     def __str__(self):
         return self.city
+
 
 class Product(models.Model):
     product = models.CharField(max_length=50)
@@ -19,10 +21,11 @@ class Retailer(models.Model):
 
     def __str__(self):
         return f'{self.company} to {self.city}'
+
+
 class Client(models.Model):
     product = models.ManyToManyField(Product)
     key = models.ForeignKey(City, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Client {self.key_id} from {self.key} buy {list(self.product.all())}'
-
