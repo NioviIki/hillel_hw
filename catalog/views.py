0b5f9921fe7fdd9e67f36620c91023ad26cc8092
niovi <prodamgarazhnedorogo@gmail.com>
2023-02-16 18:40:51 +0200

    final_form

--- catalog/views.py
+++ catalog/views.py
@@ -8,12 +8,12 @@ from .forms import Side_of_triangleForm
 def TriangleView(request):
 
     hip = None
-    x = Side_of_triangleForm(request.GET)
+    form = Side_of_triangleForm(request.GET)
 
-    if x.is_valid():
+    if form.is_valid():
         cathetuses_of_triangle = list(request.GET.dict().values())
         hip = int(sqrt(int(cathetuses_of_triangle[0]) ** 2 +
                        int(cathetuses_of_triangle[1]) ** 2))
 
     return render(request, 'catalog/Triangle.html',
-                  {'hip': hip, "q_form": Side_of_triangleForm(), "x": x})
+                  {'hip': hip, "q_form": Side_of_triangleForm(), "form": form})
