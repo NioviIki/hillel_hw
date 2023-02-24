3fb550b3c9b7937d2c24cf81a5800d8e977f3770
niovi <prodamgarazhnedorogo@gmail.com>
2023-02-24 14:57:43 +0200

    hw13 admin

--- new_hw/admin.py
+++ new_hw/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Author, Publisher, Book, Store
 
+class BookPublisherInline(admin.TabularInline):
+    model = Book
+    extra = 1
+
+class BookStoreInline(admin.TabularInline):
+    model = Store.books.through
+    extra = 1
 
 
 @admin.register(Author)
@@ -11,18 +18,20 @@ class AuthorAdmin(admin.ModelAdmin):
     list_filter = ['age']
     list_per_page = 20
 
+
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ('name', )
     list_per_page = 20
     search_fields = ['name']
+    inlines = [BookPublisherInline,]
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_per_page = 20
     list_display = ('name', )
-    readonly_fields = ('book_in_store', )
     search_fields = ('name', )
+    inlines = [BookStoreInline, ]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
